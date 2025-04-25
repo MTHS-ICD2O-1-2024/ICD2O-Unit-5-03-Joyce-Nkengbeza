d5af8d3471443398f3d39c34c40bffa9eb29ca5e
default <Joyce.nkengbeza@stu.ocsb.ca>
2025-04-25 16:23:14 +0000

    templa

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Joyce Nkengbeza
+// Created on: Mar 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function calculatearea() {
+  // input
+  const abase = parseInt(document.getElementById('a-base').value);
+  const bbase = parseInt(document.getElementById('b-base').value);
+  const height = parseInt(document.getElementById('height-length').value);
+    
+
+  // process
+    const trapezoidarea = [(abase + bbase) / 2] * height
+
+  // output
+  document.getElementById('area').innerHTML = 'area is:' + trapezoidarea + 'mm²'
+}
+
